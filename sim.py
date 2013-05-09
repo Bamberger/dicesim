c6df0446e8f0d6b54ff5b017ba7772890c058d40
Jamie Saxon <jsaxon@panviva.com>
2013-05-09 16:22:03 +1000

    Added help, flags and percentiles

--- sim.py
+++ sim.py
@@ -1,48 +1,73 @@
 # IMPORTS
 import rolls
+import percentile
 from datetime import datetime
+import argparse
 # from multiprocessing import Pool
 
 time_start = datetime.now()
 
-# VARS
-attacks = 10000000 # How many parses to run
-at = 7 # MAT/RAT of Attacker
-pow =11 # POW of Hit
-defense = 7 # DEF of Defender
-arm = 20 # ARM of Defender
-dice_hit = 2 # Number of Hit dice
-dice_dmg = 4 # Number of Damage dice
+# Predefined Variables
+attacks = 10000  # How many parses to run
+at = 7  # MAT/RAT of Attacker
+pow = 11  # POW of Hit
+defense = 14  # DEF of Defender
+arm = 20  # ARM of Defender
+dice_hit = 2  # Number of Hit dice
+dice_dmg = 4  # Number of Damage dice
 max_result = 51
 
-results = [0] * max_result  # Create an array to handle up to 'max_result' dmg
-landing_results = [] # Create an array for results to land in before sorting
+parser = argparse.ArgumentParser()
+parser.add_argument("-a","--at", help="Attackers MAT/RAT")
+parser.add_argument("-p","--pow", help="Attackers POW")
+parser.add_argument("-d","--defense", help="Defenders DEF")
+parser.add_argument("-r","--arm", help="Defenders ARM")
+parser.add_argument("-i","--hit", help="Attackers Hit Dice #")
+parser.add_argument("-m","--dmg", help="Attackers Damage Dice #")
+parser.add_argument("-c","--attacks", help="Number of Attacks to sim")
+args = parser.parse_args()
+if args.at:    
+    at = int(args.at)
+if args.pow:    
+    pow = int(args.pow)
+if args.defense:    
+    defense = int(args.defense)
+if args.arm:    
+    arm = int(args.arm)
+if args.hit:    
+    hit = int(args.hit) 
+if args.dmg:    
+    dmg = int(args.dmg)
+if args.attacks:    
+    attacks = int(args.attacks)
 
-# 
-# roll_counter = 0
-# while roll_counter < attacks:
-#     roll_counter += 1
-#     result = rolls.roll_full(at,pow,defense,arm,dice_hit,dice_dmg)
-#     results[result] += 1
+results = [0] * max_result  # Create an array to handle up to 'max_result' dmg
+landing_results = []  # Create an array for results to land in before sorting
     
 roll_counter = 0
 while roll_counter < attacks:
     roll_counter += 1
-    result = rolls.roll_full(at,pow,defense,arm,dice_hit,dice_dmg)
+    result = rolls.roll_full(at, pow, defense, arm, dice_hit, dice_dmg)
     landing_results.append(result)
-# print(landing_results)
+
+landing_results.sort()  # Order the landing_results for later use
 
 for i in landing_results:
-    results[i] += 1
+    results[i] += 1  # Populate the results array
 
 result_counter = 0
 average = 0
 while result_counter < max_result:
     if results[result_counter] != 0:
 #         print(result_counter, "damage: ", results[result_counter], "/", attacks, round(results[result_counter] / attacks * 100,2), "%")
-        print(result_counter, "damage: ", round(results[result_counter] / attacks * 100,2), "%")
-        average = average + result_counter * round(results[result_counter] / attacks ,2)
+        print(result_counter, "damage: ", round(results[result_counter] / attacks * 100, 2), "%")
+        average = average + result_counter * round(results[result_counter] / attacks , 2)
     result_counter += 1
-print("Average Damage:",round(average,2))
+print("Average Damage:", round(average, 2))
 
-print("Execution time:", datetime.now() - time_start)
+print("25th Percentile:",percentile.percentile(landing_results,0.25))
+print("50th Percentile:",percentile.percentile(landing_results,0.50))
+print("75th Percentile:",percentile.percentile(landing_results,0.75))
+print("90th Percentile:",percentile.percentile(landing_results,0.90))
+
+# print("Execution time:", datetime.now() - time_start)
